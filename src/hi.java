a6f87e062c2366f97a6dabf37b2fd8599740649c
Muhammad Usman Siddiqui <siddiquimuhammadusman@gmail.com>
2015-03-07 22:35:15 +0500

    Initializing Runtime Checking in Java Programs.

---
+++ src/hi.java
@@ -0,0 +1,15 @@
+
+
+public class hi{
+	public static void main(String[] args) {
+
+
+		System.out.println("Hi Runtime!");
+		long st=System.nanoTime();
+				
+				
+				
+		long et=System.nanoTime();
+		System.out.println("Took "+(et-st)+"ns");
+				
+	}}
